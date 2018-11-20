f23dbdb18c8909d2a22340877d04ca5232347ef2
T Thiyagaraj <tstream.h@outlook.com>
2018-11-21 02:05:36 +0530

    remove unused dependencies

--- src/App.js
+++ src/App.js
@@ -5,11 +5,6 @@ import rootReducer from './reducers';
 import BoardContainer from './containers/BoardContainer';
 import AddList from './containers/AddList';
 import './App.css';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStroopwafel } from '@fortawesome/free-solid-svg-icons';
-
-library.add(faStroopwafel);
 
 const store = createStore(
   rootReducer,
